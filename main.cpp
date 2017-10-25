4eaef42f543e0f9564b1b4193fcfbaa37a1dd339
Zenande GODONGWANA <zgodongw@e4r14p5.wethinkcode.co.za>
2017-10-25 17:01:56 +0200

    Main

--- main.cpp
+++ main.cpp
@@ -21,24 +21,29 @@ int										main(int argc, char *argv[])
 	TermUI*								ui = new TermUI();
 	std::vector<std::string>			rulearray;
 
+	PRINT CLEAR;
 	if (argc < 2) {
-		PRINT BOLD RED "No text input: "<<RESET BOLD<<"PROGRAM TERMINATED!" END;
-		return (-1);
+		PRINT BOLD RED "No text input: "<<RESET BOLD<<"Please Insert Rules:" END;
+		rulearray = s->start(ui->interact());
+		remove("temp.txt");
+	}
+	else {
+		rulearray = s->start((std::string)argv[1]);
 	}
 
 	map->initMap();
-	rulearray = s->start((std::string)argv[1]);
-
-	if (error->errorCheck(rulearray) == false) {
-		map->findfacts(rulearray);
+	if (!rulearray.empty()) {
+		if (error->errorCheck(rulearray) == false) {
+			map->findfacts(rulearray);
     
-		rule->Programloop(rulearray);
+			rule->Programloop(rulearray);
 		
-		ui->InitUI(rulearray);
+			ui->InitUI(rulearray);
 	
-		map->findquery(rulearray);
+			map->findquery(rulearray);
 		
-		ui->queryLoop();
+			ui->queryLoop();
+		}
 	}
 
 	delete s;
