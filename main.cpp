a01685ce0874704ca05e42e37d3aa66a66b04902
Zenande GODONGWANA <zgodongw@e4r14p5.wethinkcode.co.za>
2017-10-30 08:39:34 +0200

    Main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: zgodongw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 14:10:05 by zgodongw          #+#    #+#             */
-/*   Updated: 2017/10/25 09:48:36 by zgodongw         ###   ########.fr       */
+/*   Updated: 2017/10/28 12:20:50 by zgodongw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,18 @@ int										main(int argc, char *argv[])
 	Error*								error = new Error();
 	TermUI*								ui = new TermUI();
 	std::vector<std::string>			rulearray;
+	std::vector<std::string>			array2;
 
 	PRINT CLEAR;
 	if (argc < 2) {
 		PRINT BOLD RED "No text input: "<<RESET BOLD<<"Please Insert Rules:" END;
-		rulearray = s->start(ui->interact());
+		rulearray = s->start(ui->interact(), false);
+		array2 = s->start("temp.txt", true);
 		remove("temp.txt");
 	}
 	else {
-		rulearray = s->start((std::string)argv[1]);
+		rulearray = s->start((std::string)argv[1], false);
+		array2 = s->start((std::string)argv[1], true);
 	}
 
 	map->initMap();
@@ -38,7 +41,7 @@ int										main(int argc, char *argv[])
     
 			rule->Programloop(rulearray);
 		
-			ui->InitUI(rulearray);
+			ui->InitUI(array2);
 	
 			map->findquery(rulearray);
 		
--- main.hpp
+++ main.hpp
@@ -18,25 +18,27 @@
 # include <string>
 # include <fstream>
 # include <cstdlib>
+# include <cstdio>
+#include <unistd.h>
 # include <map>
 
-# define CLEAR		"\33[H\33[2J"
-# define RED		"\x1b[31m"
-# define GREY		"\x1b[37m"
-# define GREEN		"\x1b[32m"
-# define YELLOW		"\x1b[33m"
-# define BLUE		"\x1b[34m"
-# define LBLUE		"\x1b[36m"
-# define MAGENTA	"\x1b[35m"
-# define CYAN		"\033[0;36m"
-# define RESET		"\x1b[0m"
-# define BOLD		"\033[1m"
-# define LPURP		"\x1b[1;35m"
+# define CLEAR	    	"\33[H\33[2J"
+# define RED	    	"\x1b[31m"
+# define GREY	    	"\x1b[37m"
+# define GREEN	    	"\x1b[32m"
+# define YELLOW	    	"\x1b[33m"
+# define BLUE	    	"\x1b[34m"
+# define LBLUE	    	"\x1b[36m"
+# define MAGENTA    	"\x1b[35m"
+# define CYAN	    	"\033[0;36m"
+# define RESET		    "\x1b[0m"
+# define BOLD		    "\033[1m"
+# define LPURP		    "\x1b[1;35m"
 
-# define PRINT		std::cout<<
-# define END		<<RESET<<std::endl
+# define PRINT		    std::cout<<
+# define END		    <<RESET<<std::endl
 
-std::map<char, bool> fact;
+std::map<char, int>    fact;
 
 # include "classes/Log.hpp"
 # include "classes/InstructionMap.hpp"
