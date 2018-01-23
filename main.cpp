792c856d45aad33ec44aeceae12df70a5e0577f5
Zenande GODONGWANA <zgodongw@e4r14p5.wethinkcode.co.za>
2018-01-23 17:40:49 +0200

    Fixed an issue

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: zgodongw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 14:10:05 by zgodongw          #+#    #+#             */
-/*   Updated: 2017/10/28 12:20:50 by zgodongw         ###   ########.fr       */
+/*   Updated: 2018/01/23 17:39:57 by zgodongw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,40 @@
 
 int										main(int argc, char *argv[])
 {
-	Setup*								s = new Setup();
-	InstructionMap*	 					map = new InstructionMap();
-	Rules*								rule = new Rules();
-	Error*								error = new Error();
-	TermUI*								ui = new TermUI();
+	Setup								s;
+	InstructionMap	 					map;
+	Rules								rule;
+	Error								error;
+	TermUI								ui;
 	std::vector<std::string>			rulearray;
 	std::vector<std::string>			array2;
 
 	PRINT CLEAR;
 	if (argc < 2) {
 		PRINT BOLD RED "No text input: "<<RESET BOLD<<"Please Insert Rules:" END;
-		rulearray = s->start(ui->interact(), false);
-		array2 = s->start("temp.txt", true);
+		rulearray = s.start(ui.interact(), false);
+		array2 = s.start("temp.txt", true);
 		remove("temp.txt");
 	}
 	else {
-		rulearray = s->start((std::string)argv[1], false);
-		array2 = s->start((std::string)argv[1], true);
+		rulearray = s.start((std::string)argv[1], false);
+		array2 = s.start((std::string)argv[1], true);
 	}
 
-	map->initMap();
+	map.initMap();
 	if (!rulearray.empty()) {
-		if (error->errorCheck(rulearray) == false) {
-			map->findfacts(rulearray);
+		if (error.errorCheck(rulearray) == false) {
+			map.findfacts(rulearray);
     
-			rule->Programloop(rulearray);
+			rule.Programloop(rulearray);
 		
-			ui->InitUI(array2);
+			ui.InitUI(array2);
 	
-			map->findquery(rulearray);
+			map.findquery(rulearray);
 		
-			ui->queryLoop();
+			ui.queryLoop();
 		}
 	}
-
-	delete s;
-	delete map;
-	delete rule;
-	delete error;
-	delete ui;
 
 	return (0);
 }
